27d0e13cea4f90398c276551fcea9add254de9b3
sldrcyang <67252954+sldrcyang@users.noreply.github.com>
2023-03-27 21:32:57 +0800

    Update example.R

--- example/example.R
+++ example/example.R
@@ -1,3 +1,6 @@
+rm(list = ls())
+setwd("your/path")
+
 library(pls)
 library(glmnet)
 library(hdi)
@@ -16,8 +19,30 @@ exposure_matrix0 = scale(exposure_matrix0)
 outcome = scale(outcome)
 
 
-#if gwas summary statistics used to selected IVs is based on another independent dataset, then wcc = F
-mr_result = mr_pl(g_matrix0, exposure_matrix0, outcome, gwas_assoc, cutoff=5e-8, wcc = T, c=20, pleiotropy_test = T)
+#cutoff: the p-value threshold used to select instrumental snps from gwas_assoc.txt
+#wcc: whether to perform winner's curse correction
+#c: the tuning parameter in the winner's curse correction, which is recommended to set as 20; if wcc = FALSE, then c = 0
+#pleiotropy_testwhether to perform pleiotropy test
+
+#if gwas_assoc.txt used to selected IVs is based on the same dataset
+mr_result = mr_pl(g_matrix0, 
+                  exposure_matrix0, 
+                  outcome, 
+                  gwas_assoc, 
+                  cutoff = 5e-8, 
+                  wcc = TRUE, 
+                  c = 20, 
+                  pleiotropy_test = TRUE)
+#if gwas_assoc.txt used to selected IVs is based on another independent dataset
+mr_result = mr_pl(g_matrix0, 
+                  exposure_matrix0, 
+                  outcome, 
+                  gwas_assoc, 
+                  cutoff = 5e-8, 
+                  wcc = FALSE, 
+                  c = 0, 
+                  pleiotropy_test = TRUE)
+
 
 #exposures with non-zero causal estimate and its corresponding p-value from lasso projection method
 mr_result$main_results
@@ -27,3 +52,4 @@ mr_result$pleiotropy_test.p   #if p < 0.05, there exists horizontal pleiotropy
 mr_result$iv_include
 #the exposures used for MR analysis (after winner's curse correction)
 mr_result$exposure_include
+
