535db2c06fa53494d64d5b887f3709a4529ee62a
sldrcyang <67252954+sldrcyang@users.noreply.github.com>
2023-09-13 20:14:19 +0800

    Update 2.4.PLS+feature selection.R

--- reproduce/simulation/supplementary simulation with the presence of pleiotropy/2.4.PLS+feature selection.R
+++ reproduce/simulation/supplementary simulation with the presence of pleiotropy/2.4.PLS+feature selection.R
@@ -22,7 +22,7 @@ pred_pls = function(plsdata1_Z, plsdata2_Z){
   pls.fit = plsr(plsdata2_Z ~ plsdata1_Z, ncomp = 20, validation="CV", jackknife=TRUE)
   
   RMSE = RMSEP(pls.fit)
-  best_ncomp = which.min(RMSE$val [2,1,-1])
+  best_ncomp = which.min(apply(RMSE$val [2,1:20,-1], 2, mean))
   cat(best_ncomp, '\n')
   
   pls.fit = plsr(plsdata2_Z ~ plsdata1_Z, ncomp = best_ncomp, validation="none")
